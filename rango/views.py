b714d9e8547a00c795b8e747b6e0e2b0a174b0c4
Joachim Vanneste <2460800V@student.gla.ac.uk>
2021-02-05 16:31:28 +0000

    chapter 9 done

--- rango/views.py
+++ rango/views.py
@@ -7,6 +7,9 @@ from rango.forms import PageForm
 from django.shortcuts import redirect
 from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
 
 
 def index(request):
@@ -43,46 +46,51 @@ def show_category(request, category_name_slug):
 
 
 def add_category(request):
-	form = CategoryForm()
+	if request.user.is_authenticated:
+		form = CategoryForm()
+		if request.method == 'POST':
+			form= CategoryForm(request.POST)
 
-	if request.method == 'POST':
-		form= CategoryForm(request.POST)
-
-		if form.is_valid():
-			form.save(commit = True)
-			return redirect('/rango/')
-		else:
-			print(form.errors)
-	return render(request, 'rango/add_category.html', {'form':form})
+			if form.is_valid():
+				form.save(commit = True)
+				return redirect('/rango/')
+			else:
+				print(form.errors)
+		return render(request, 'rango/add_category.html', {'form':form})
+	else:
+		return redirect(reverse('rango:login'))
 
 
 
 def add_page(request, category_name_slug):
-	try:
-		category = Category.objects.get(slug = category_name_slug)
-	except Category.DoesNotExist:
-		category = None
+	if request.user.is_authenticated:
+		try:
+			category = Category.objects.get(slug = category_name_slug)
+		except Category.DoesNotExist:
+			category = None
 
-	if category is None:
-		return redirect('/rango/')
+		if category is None:
+			return redirect('/rango/')
 
-	form = PageForm()
+		form = PageForm()
 
-	if request.method == 'POST':
-		form = PageForm(request.POST)
+		if request.method == 'POST':
+			form = PageForm(request.POST)
 
-		if form.is_valid():
-			if category:
-				page = form.save(commit=False)
-				page.category = category
-				page.views = 0
-				page.save()
+			if form.is_valid():
+				if category:
+					page = form.save(commit=False)
+					page.category = category
+					page.views = 0
+					page.save()
 
-				return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
+					return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
 
 
-		else:
-			print(form.errors)
+			else:
+				print(form.errors)
+	else:
+		return redirect(reverse('rango:login'))
 
 
 	context_dict = {'form':form, 'category': category}
@@ -118,6 +126,41 @@ def register(request):
 	return render(request, 'rango/register.html', context = {'user_form':user_form,
 															 'profile_form':profile_form, 
 															 'registered':registered})
+
+
+
+def user_login(request):
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+
+		user = authenticate(username=username, password=password)
+
+		if user:
+			if user.is_active:
+				login(request, user)
+				return redirect(reverse('rango:index'))
+			else:
+				return HttpResponse("Your Rango account is disabled.")
+		else:
+			print(f"Invalid login details: {username}, {password}")
+			return HttpResponse("Invalid login details supplied.")
+	else:
+		return render(request, 'rango/login.html')
+
+
+
+@login_required
+def restricted(request):
+	return render(request, 'rango/restricted.html')
+
+@login_required
+def user_logout(request):
+	logout(request)
+	return redirect(reverse('rango:index'))
+
+
+
 
 
 
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
+LOGIN_URL = 'rango:login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
